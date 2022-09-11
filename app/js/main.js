6de64e4f8b6c71c1641e96e4261482dac5000e61
Rizvan <xl92@mail.ru>
2022-09-11 13:35:52 +0300

    finish header

---
+++ app/js/main.js
@@ -0,0 +1,28 @@
+window.addEventListener('DOMContentLoaded', function () {
+    'use strict';
+
+
+/*     const li = document.querySelector('#header__li'),
+        ul = document.querySelector('#header__ul');
+
+    li.addEventListener('mouseover', function() {
+        if(! ul.classList.contains('active')) {
+            ul.classList.add('active');
+        }
+
+
+        ul.addEventListener('mouseout', () => {
+            setTimeout(function() {
+                ul.classList.remove('active');
+            }, 3000);
+        });
+
+    }); */
+
+
+
+
+
+
+
+});
