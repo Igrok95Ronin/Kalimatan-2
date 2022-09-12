18fe0815482e79c8f190a9bc653df5204c02247e
Rizvan <xl92@mail.ru>
2022-09-12 09:51:22 +0300

    finish main-slider section

--- app/js/main.js
+++ app/js/main.js
@@ -1,25 +1,20 @@
 window.addEventListener('DOMContentLoaded', function () {
     'use strict';
 
-
-/*     const li = document.querySelector('#header__li'),
-        ul = document.querySelector('#header__ul');
-
-    li.addEventListener('mouseover', function() {
-        if(! ul.classList.contains('active')) {
-            ul.classList.add('active');
-        }
-
-
-        ul.addEventListener('mouseout', () => {
-            setTimeout(function() {
-                ul.classList.remove('active');
-            }, 3000);
-        });
-
-    }); */
-
-
+    $(document).ready(function () {
+        $('.main-slider__box').slick(
+            {
+                dots: true,
+                infinite: true,
+                speed: 500,
+                // fade: true,
+                arrows: false,
+                autoplay: true,
+                autoplaySpeed: 5000
+              }
+        );
+    });
+//
 
 
 
